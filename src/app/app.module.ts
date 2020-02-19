b6b70428ae503ede04dd0db14cfd5f14f39800ab
VishnuNagesh <56096916+VishnuNagesh@users.noreply.github.com>
2020-02-19 12:33:08 +0530

    implement login page design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule , ReactiveFormsModule}   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ɵConsole } from '@angular/core';
+import { Login } from '../service/login';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+
+  modelLogin = new Login(null, null);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  loginForm = new FormGroup({
+    Username : new FormControl(),
+    Password : new FormControl ()
+  });
+
+  onSubmit(heroForm) : void {
+    console.log("onsubmit");
+    console.log(this.UserName.value);
+    console.log(this.Password.value);
+  }
+
+  get UserName() {
+    return this.loginForm.get('Username');
+  }
+
+  get Password() {
+    return this.loginForm.get('Password');
+  }
+}
