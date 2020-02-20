fcb9429d9479bc185a9630c18bfa97754f99a5b6
dareljones <46834627+dareljones@users.noreply.github.com>
2020-02-20 13:51:44 +0530

    modified layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { BookingComponent } from './booking/booking.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginPageComponent }
+  { path: '', component: LoginPageComponent } ,
+  { path: 'login', component: LoginPageComponent },
+  { path: 'booking', component: BookingComponent }
+
 ];
 
 @NgModule({
